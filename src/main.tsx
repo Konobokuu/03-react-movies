86dab14c4c03b40d1be7de0b22569eef30a56ef5
Oleksandr <kksasha201507@gmail.com>
2026-05-13 19:24:36 +0200

    fix-app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import 'modern-normalize/modern-normalize.css';
 import "./index.css";
 
-import App from "./App";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
